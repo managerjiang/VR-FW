efdca3f9489f4cc49b607ca0df871dbce81b97b4
managerjiang <jx13032828@163.com>
2022-04-12 09:21:03 +0800

    Add files via upload

---
+++ convex/a9a/cen_spiderfw.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  8 22:10:53 2021
+
+@author: JX
+"""
+
+from __future__ import division 
+#fruture前后是两个下划线：__future__
+#上述是为了希望只执行简单除法，例如1/2=0.5这样的,单斜线不再用作整除，双斜线实现整除操作
+import numpy as np
+from scipy.io import savemat
+import scipy.io as scio
+import math
+import matplotlib.pyplot as plt
+import time
+ ##邻接矩阵
+dataFile = 'F:/anaconda_spyder/data/C_meth1_smote_800.mat'
+data = scio.loadmat(dataFile)
+C=data['a']
+#print(C[2][3])
+##数据
+a9asam=scio.loadmat('F:/anaconda_spyder/data/a9a/a9a_smote.mat') #载入A：A(48243x123):48243个数据
+a9al=scio.loadmat('F:/anaconda_spyder/data/a9a/L_a9a_smote.mat') #载入L：A(1x48243):48243个结果
+'''
+a9asam=scio.loadmat('F:/anaconda_spyder/data/w8a/w8a_smote.mat') #载入A：A(48243x123):48243个数据
+a9al=scio.loadmat('F:/anaconda_spyder/data/w8a/L_w8a_smote.mat') #载入L：A(1x48243):48243个结果
+'''
+A=a9asam['A1'];
+L=a9al['L1'];
+L=L[0]
+'''
+a9asam=scio.loadmat('F:/anaconda_spyder/data/covtype/cov.mat') #载入A：A(48243x123):48243个数据
+a9al=scio.loadmat('F:/anaconda_spyder/data/covtype/L_cov.mat') #载入L：A(1x48243):48243个结果
+A=a9asam['A'];
+L=a9al['L'];
+L=L[0]
+r={1:-1,2:1}
+L=[r[i] if i in r else i for i in L]
+'''
+#print(np.size(A,0))
+#智能体数目和总样本数
+agent_n=10
+sample_n=np.size(A,0)
+
+x_dim=np.size(A,1)#变量维度
+start = time.time()
+#变量初始化
+listx_i=[]
+X_i=np.ones(x_dim)
+X_i=X_i/(np.linalg.norm(X_i)) 
+barX_i=X_i
+listx_i.append(X_i) 
+#print("process {} initial x {}".format(rank,listx_i[0]))
+
+#梯度函数
+def gradient_com(vari_x):
+    gradient_vari_x=[0.0]*x_dim
+    for j in range(sample_n):
+        mid=L[j]*A[j,:]
+        #print(mid@vari_x)
+        gradient_x=-mid*math.exp(-mid@vari_x)/(1+math.exp(-mid@vari_x));
+        gradient_vari_x=gradient_vari_x+gradient_x
+    return gradient_vari_x/sample_n
+
+def gradient_com_set(vari_x,S):
+    gradient_vari_x=[0.0]*x_dim
+    for j in S:
+        mid=L[j]*A[j,:]
+        gradient_x=-mid*math.exp(-mid@vari_x)/(1+math.exp(-mid@vari_x));
+        gradient_vari_x=gradient_vari_x+gradient_x
+    return gradient_vari_x
+
+#print("process {} initial grad {}".format(rank,gradientx_i_seq[0]))
+v0=gradient_com(X_i)
+#开始迭代
+T=300
+K=math.ceil(sample_n**0.5)###参数
+c_k=2
+q=K
+R=20
+#eta=1/(T**0.5)## 1/(c_k*T**0.5)
+for t in range(T):
+    #eta=2/(t+1)
+    k=0
+    #eta=2/(K+t+1)
+    eta=2/(2**k+t)
+    if t%q==0: #########
+		#K=2**(t+1)
+        if k!=0:
+            k=k+1
+        v1=gradient_com(X_i)
+    else:
+        S=np.random.choice(sample_n, K)
+        v1=(gradient_com_set(listx_i[t],S)-gradient_com_set(listx_i[t-1],S))/K+v0       
+    #resul=-c_k/(2*np.linalg.norm(v1))*v1
+    sumd_abs=list(map(abs,v1))
+    maxk=sumd_abs.index(max(sumd_abs))
+    resul=np.zeros(x_dim)  #.reshape(-1,1)
+    resul[maxk]=-R*np.sign(v1[maxk]) 
+    X_i=(1-eta)*listx_i[t]+eta*resul
+    v0=v1
+    listx_i.append(X_i)
+
+
+#迭代结果序列（函数值和一致性结果）10 T+1 123
+'''
+    print('rank {} is X_col is {}'.format(rank, X_col[1][1]))
+    print('X_col dim 0 is {}'.format(np.size(X_col,0)))
+    print('X_col dim 1 is {}'.format(np.size(X_col,1)))
+    print('X_col dim 2 is {}'.format(np.size(X_col,2)))
+'''
+
+end = time.time()
+runntime=end-start
+print(runntime)
+
+X_col=np.array(listx_i)
+
+file_name='CENSPIDER.mat'
+savemat(file_name,{'runtime_censpider':runntime,'x_col_censpider':X_col})
+
+'''    
+    consenseq=[]
+    fvseq=[]
+    for t in range(T):
+        fv=0.0
+        for i in range(sample_n):
+            fv=fv+np.log(1+np.exp(-L[i]*A[i,:]@X_col[1][t]))
+        #print('rank {} coll fv is {}'.format(rank,fv))
+        fvseq.append(fv/sample_n)
+        consen=0;
+        for i in range(agent_n):
+            xcon=[0]*x_dim
+            for j in range(agent_n):
+                xcon=xcon+C[i][j]*(X_col[i][t]-X_col[j][t])
+            xdot=X_col[i][t]@xcon
+            consen=consen+xdot
+        #print('rank {} coll consensus is {}'.format(rank,consen))
+        consenseq.append(consen) 
+    #制图
+    fig=plt.figure()
+    ax=fig.add_subplot(1,2,1)
+    ax.plot(range(T),consenseq)
+    ax=fig.add_subplot(1,2,2)
+    ax.plot(range(T),fvseq)
+    #ax.plot(range(0,T,10),fvseq[::10],color='r', marker='o', markerfacecolor='blue', markersize=5)
+    #for a,b in zip(range(0,T,10),fvseq[::10]):
+    #    ax.text(a,b,b,ha='center',va='bottom',fontsize=8)
+    plt.show()
+'''
---
+++ convex/a9a/drawfig.m
@@ -0,0 +1,119 @@
+close all;
+clc;
+clear;
+load('F:/anaconda_spyder/data/C_meth1_smote_800.mat')
+load('GTTAC.mat');%加载保存的迭代梯度信息x_k_store{}
+load('GTSPIDER.mat');%加载保存的迭代解信息gradient()
+load('CENSPIDER.mat');%加载保存的邻接矩阵信息C_store
+
+load('F:/anaconda_spyder/data/a9a/a9a_smote.mat');
+load('F:/anaconda_spyder/data/a9a/L_a9a_smote.mat');
+A=double(A1);
+L=double(L1);
+L(L==0)=-1;
+C=a;
+c_k=2;
+R=20;
+Maxgen1=size(x_col_spider,2)-1;%迭代次数
+x_dim=size(x_col_spider,3);
+agent_num=size(C,1);%智能体个数
+
+for k=1:Maxgen1
+    x_k_agent=x_col_spider(:,k,:);
+    x_k_agent=reshape(x_k_agent,agent_num,x_dim);
+    x_k_1=x_k_agent(1,:);
+    Xave=sum(x_k_agent,1)/agent_num;
+    fi=sum(log((1+exp(-L'.*A*Xave'))),1);
+    obj_m1(k)=fi/size(A,1);
+     XLX=0;
+  for i=1:agent_num
+    temp_X=zeros(x_dim,1);
+    for j=1:agent_num
+        temp_X=temp_X+C(i,j)*(x_k_agent(i,:)'-x_k_agent(j,:)');
+    end
+    XLX=XLX+x_k_agent(i,:)*temp_X;
+  end
+  w_m1s(k)=XLX;
+  gg=sum(-L'.*A.*exp(-L'.*A*Xave')./(1+exp(-L'.*A*Xave')),1)/size(A,1);
+  %resul=-c_k/(2*norm(gg))*gg;
+  sumd_abs=abs(gg);
+  maxk=find(sumd_abs==max(sumd_abs));
+  resul=zeros(1,x_dim);
+  resul(maxk)=-R*sign(gg(maxk));
+  
+  Fgap=gg*(Xave-resul)';
+  fwgap1(k)=log10(Fgap);
+  const1(k)=norm(Xave);
+end
+%%meth tac
+for k=1:Maxgen1
+    x_k_agent=x_col_tac(:,k,:);
+    x_k_agent=reshape(x_k_agent,agent_num,x_dim);
+    x_k_1=x_k_agent(1,:);
+    Xave=sum(x_k_agent,1)/agent_num;
+    fi=sum(log((1+exp(-L'.*A*Xave'))),1);
+    obj_m2(k)=fi/size(A,1);
+     XLX=0;
+  for i=1:agent_num
+    temp_X=zeros(x_dim,1);
+    for j=1:agent_num
+        temp_X=temp_X+C(i,j)*(x_k_agent(i,:)'-x_k_agent(j,:)');
+    end
+    XLX=XLX+x_k_agent(i,:)*temp_X;
+  end
+  w_m2s(k)=XLX;
+  gg=sum(-L'.*A.*exp(-L'.*A*Xave')./(1+exp(-L'.*A*Xave')),1)/size(A,1);
+  %resul=-c_k/(2*norm(gg))*gg;
+  sumd_abs=abs(gg);
+  maxk=find(sumd_abs==max(sumd_abs));
+  resul=zeros(1,x_dim);
+  resul(maxk)=-R*sign(gg(maxk));
+  
+  Fgap=gg*(Xave-resul)';
+  fwgap2(k)=log10(Fgap);
+  const2(k)=norm(Xave);
+end
+
+
+%%meth censpider
+for k=1:Maxgen1
+    x_k_1=x_col_censpider(k,:);
+    fi=sum(log((1+exp(-L'.*A*x_k_1'))),1);
+    obj_m3(k)=fi/size(A,1);
+  gg=sum(-L'.*A.*exp(-L'.*A*x_k_1')./(1+exp(-L'.*A*x_k_1')),1)/size(A,1);
+  %resul=-c_k/(2*norm(gg))*gg;
+  sumd_abs=abs(gg);
+  maxk=find(sumd_abs==max(sumd_abs));
+  resul=zeros(1,x_dim);
+  resul(maxk)=-R*sign(gg(maxk));
+  
+  Fgap=gg*(x_k_1-resul)';
+  fwgap3(k)=log10(Fgap);
+  const3(k)=norm(x_k_1);
+end
+figure(1);
+tt=1:5:Maxgen1;
+plot(tt(1:2:length(tt)),obj_m1(tt(1:2:length(tt))),'r-x','linewidth',1),hold on;
+plot(tt(1:2:length(tt)),obj_m2(tt(1:2:length(tt))),'k-^','linewidth',1),hold on;
+plot(tt(1:2:length(tt)),obj_m3(tt(1:2:length(tt))),'b-o','linewidth',1),hold on;
+legend("FontName","Times New Roman","FontSize",14); 
+legend('DstoFW','DenFW','CenFW');
+ylabel('$$F(\bar{x}^k)$$','Interpreter','latex',"FontSize",17)
+xlabel('k',"FontSize",17);
+
+figure(2);
+plot(tt(1:2:length(tt)),fwgap1(tt(1:2:length(tt))),'r-x','linewidth',1),hold on;
+plot(tt(1:2:length(tt)),fwgap2(tt(1:2:length(tt))),'k-^','linewidth',1),hold on;
+plot(tt(1:2:length(tt)),fwgap3(tt(1:2:length(tt))),'b-o','linewidth',1),hold on;
+legend("FontName","Times New Roman","FontSize",14); 
+legend('DstoFW','DenFW','CenFW');
+ylabel('$$\log g_k$$','Interpreter','latex',"FontSize",17)
+xlabel('k',"FontSize",17);
+
+% figure(3);
+% plot(1:Maxgen1,const1,'r','linewidth',1),hold on;
+% plot(1:Maxgen1,const2,'k-.','linewidth',1),hold on;
+% plot(1:Maxgen1,const3,'b--','linewidth',1),hold on;
+% legend('DstoFW','DenFW','CenFW');
+% ylabel('$$\Omega$$','Interpreter','latex')
+% xlabel('iterations');
